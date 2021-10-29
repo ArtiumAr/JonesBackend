4cd022e5d963be555bbfc3dd960e4e939d5bdc09
tioma <artiumar@post.bgu.ac.il>
2021-10-29 22:40:53 +0300

    added basic views

--- restaurant_api/api_views.py
+++ restaurant_api/api_views.py
@@ -1,17 +1,29 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from restaurant_api.database.models import Order
-
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 from restaurant_api.database import db
 bp = Blueprint('api_views', __name__, url_prefix='/api')
+
+sql = SQLAlchemy()
 
 @bp.route('/place-order', methods=['POST'])
 def order():
     json = request.get_json(force=True)
-    order = Order(id=json['id'], customer_name=json['name'], dish=json['dish'], comments=json['comments'])
-    # order_confirmation = {}
-    # return order_confirmation
-    pass
+    id = json['id']
+    customer_name=json['customer_name']
+    dish=json['dish']
+    comments=json['comments']
+    order = Order(id=id, customer_name=customer_name, dish=dish, comments=comments)
+    db.session.add(order)
+    db.session.commit()
+    my_json = {"order status": "success", "order details": {"id":id, "customer_name":customer_name, "dish":dish, "comments":comments}}
+    http_code = 200
+    return my_json, http_code
 
 @bp.route('/orders', methods=['GET'])
 def get_orders():
-    return {}
+    s = datetime.datetime.strftime(datetime.datetime.now()-datetime.timedelta(1),'%Y-%m-%d')
+    result = db.session.query(Order)\
+    .filter(Order.order_time>= s).all()
+    return jsonify(result)
--- restaurant_api/database/models.py
+++ restaurant_api/database/models.py
@@ -1,12 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from dataclasses import dataclass
 
 db = SQLAlchemy()
 
+@dataclass
 class Order(db.Model):
+    id:int
+    customer_name:str
+    dish:str
+    comments:str
+    order_time:datetime
+
     id = db.Column(db.Integer, primary_key=True)
     customer_name = db.Column(db.String(80), unique=True, nullable=False)
     dish = db.Column(db.String(120), unique=True, nullable=False)
     comments = db.Column(db.String(120), unique=True, nullable=False)
+    order_time = db.Column(db.DateTime(), index=True, default=datetime.now())
 
     def __repr__(self):
-        return "Order {}:\n\tcustomer : {}\n\tdish:{}\n\tcomments:{}".format(self.id, self.customer_name, self.dish, self.comments)
+        return "Order {}:\n\tcustomer : {}\n\tdish:{}\n\tcomments:{}\n\torder time:{}".format(self.id, self.customer_name, self.dish, self.comments, self.order_time)
