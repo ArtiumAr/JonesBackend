31dccb90547163c0b539fb4424b6b8b6a0740946
tioma <artiumar@post.bgu.ac.il>
2021-10-30 19:13:03 +0300

    added some tests

--- restaurant_api/api_views.py
+++ restaurant_api/api_views.py
@@ -7,17 +7,17 @@ bp = Blueprint('api_views', __name__, url_prefix='/api')
 
 sql = SQLAlchemy()
 
+
 @bp.route('/place-order', methods=['POST'])
 def order():
     json = request.get_json(force=True)
-    id = json['id']
     customer_name=json['customer_name']
     dish=json['dish']
     comments=json['comments']
-    order = Order(id=id, customer_name=customer_name, dish=dish, comments=comments)
+    order = Order(customer_name=customer_name, dish=dish, comments=comments)
     db.session.add(order)
     db.session.commit()
-    my_json = {"order status": "success", "order details": {"id":id, "customer_name":customer_name, "dish":dish, "comments":comments}}
+    my_json = {"order status": "success", "order details": {"id":order.id, "customer_name":customer_name, "dish":dish, "comments":comments}}
     http_code = 200
     return my_json, http_code
 
--- restaurant_api/database/models.py
+++ restaurant_api/database/models.py
@@ -13,10 +13,10 @@ class Order(db.Model):
     order_time:datetime
 
     id = db.Column(db.Integer, primary_key=True)
-    customer_name = db.Column(db.String(80), unique=True, nullable=False)
-    dish = db.Column(db.String(120), unique=True, nullable=False)
-    comments = db.Column(db.String(120), unique=True, nullable=False)
-    order_time = db.Column(db.DateTime(), index=True, default=datetime.now())
+    customer_name = db.Column(db.String(80), unique=False, nullable=False)
+    dish = db.Column(db.String(120), unique=False, nullable=False)
+    comments = db.Column(db.String(120), unique=False, nullable=False)
+    order_time = db.Column(db.DateTime(), index=False, default=datetime.now())
 
     def __repr__(self):
         return "Order {}:\n\tcustomer : {}\n\tdish:{}\n\tcomments:{}\n\torder time:{}".format(self.id, self.customer_name, self.dish, self.comments, self.order_time)
